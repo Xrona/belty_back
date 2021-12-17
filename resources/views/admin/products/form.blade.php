98af46c557cf15e2c2ede8ba1598b219387432a0
erxweo <yizzr81@gmail.com>
2021-12-17 13:05:00 +0500

    fix create product

--- resources/views/admin/products/form.blade.php
+++ resources/views/admin/products/form.blade.php
@@ -1,3 +1,7 @@
+@php(
+    $item = $product ?? null
+)
+
 <div class="form-group {{ $errors->has('name') ? 'has-error' : ''}}">
     <label for="name" class="control-label">name</label>
     <input class="form-control" name="name" type="text" id="name" value="{{ $product->name ?? ''}}">
@@ -14,38 +18,45 @@
     {!! $errors->first('price', '<p class="help-block">:message</p>') !!}
 </div>
 
-<x-select2  name="category_id" :list="$categories" :currentValue="$product->category_id" label="Category"/>
-{!! $errors->first('category', '<p class="help-block">:message</p>') !!}
+@if($item != null)
+
+    <x-select2 name="category_id" :list="$categories" :currentValue="$item->category_id" label="Category"/>
+    {!! $errors->first('category', '<p class="help-block">:message</p>') !!}
 
 
-<x-select2  name="material_id" :list="$materials" :currentValue="$product->material_id" label="Material"/>
-{!! $errors->first('material', '<p class="help-block">:message</p>') !!}
+    <x-select2 name="material_id" :list="$materials" :currentValue="$item->material_id" label="Material"/>
+    {!! $errors->first('material', '<p class="help-block">:message</p>') !!}
 
 
-<x-select2  name="country_id" :list="$countries" :currentValue="$product->country_id" label="Country"/>
-{!! $errors->first('country', '<p class="help-block">:message</p>') !!}
+    <x-select2 name="country_id" :list="$countries" :currentValue="$item->country_id" label="Country"/>
+    {!! $errors->first('country', '<p class="help-block">:message</p>') !!}
+
+{{--    <x-select2-multiple name="sizes[]" :list="$sizes" :currentValues="$item-sizes" label="Sizes"/>--}}
+{{--    {!! $errors->first('Sizes', '<p class="help-block">:message</p>') !!}--}}
+
+@else
+
+    <x-select2 name="category_id" :list="$categories" :currentValue="null" label="Category"/>
+    {!! $errors->first('category', '<p class="help-block">:message</p>') !!}
+
+
+    <x-select2 name="material_id" :list="$materials" :currentValue="null" label="Material"/>
+    {!! $errors->first('material', '<p class="help-block">:message</p>') !!}
+
+
+    <x-select2 name="country_id" :list="$countries" :currentValue="null" label="Country"/>
+    {!! $errors->first('country', '<p class="help-block">:message</p>') !!}
+
+{{--    <x-select2-multiple name="sizes[]" :list="$sizes" :currentValues="null" label="Sizes"/>--}}
+{{--    {!! $errors->first('Sizes', '<p class="help-block">:message</p>') !!}--}}
+
+@endif
 
 <div class="form-group {{ $errors->has('status') ? 'has-error' : ''}}">
     <label for="status" class="control-label">status</label>
     <input class="form-control" name="status" type="text" id="status" value="{{ $product->status ?? ''}}">
     {!! $errors->first('status', '<p class="help-block">:message</p>') !!}
 </div>
-<div class="form-group">
-    <label>Minimal</label>
-    <div class="select2-purple">
-        <select class="js-example-basic-single" style="width: 100%;">
-            <option selected="selected">Alabama</option>
-            <option>Alaska</option>
-            <option>California</option>
-            <option>Delaware</option>
-            <option>Tennessee</option>
-            <option>Texas</option>
-            <option>Washington</option>
-        </select>
-    </div>
-
-</div>
-
 
 <div class="form-group">
     <input class="btn btn-primary" type="submit" value="{{ $formMode === 'edit' ? 'Update' : 'Create' }}">

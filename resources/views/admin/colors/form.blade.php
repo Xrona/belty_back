090e8f2427d9b601cf6c7da5d26666c3ace40294
erxweo <yizzr81@gmail.com>
2022-06-04 14:17:32 +0400

    fix colors

--- resources/views/admin/colors/form.blade.php
+++ resources/views/admin/colors/form.blade.php
@@ -1,15 +1,19 @@
-
+<?php
+if (!isset($color) {
+    $color = null;
+}
+?>
 <div class="form-group {{ $errors->has('label') ? 'has-error' : ''}}">
     <div class="col-sm-4">
         <label for="label" class="control-label">Label</label>
-        <input class="form-control" name="label" type="text" id="name" value="{{ $color->label ?? ''}}" >
+        <input class="form-control" name="label" type="text" id="name" value="{{ $color?->label ?? ''}}">
     </div>
     {!! $errors->first('label', '<p class="help-block">:message</p>') !!}
 </div>
 <div class="form-group {{ $errors->has('name') ? 'has-error' : ''}}">
     <div class="col-sm-4">
         <label for="name" class="control-label">Name</label>
-        <input class="form-control color-picker" name="name" type="text" id="name" value="{{ $color->name }}" >
+        <input class="form-control color-picker" name="name" type="text" id="name" value="{{ $color?->name }}">
     </div>
     {!! $errors->first('name', '<p class="help-block">:message</p>') !!}
 </div>

a8eb5cbc9e20945a77d7890c7331975bf4d9c9d2
erxweo <yizzr81@gmail.com>
2022-06-23 04:55:54 +0400

    fix roles

--- app/Models/User.php
+++ app/Models/User.php
@@ -52,8 +52,4 @@ class User extends Authenticatable implements JWTSubject
     {
         return [];
     }
-
-    public function roles() {
-        return $this->belongsToMany(Role::class,'users_roles');
-    }
 }

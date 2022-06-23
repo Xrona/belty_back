b8fc2b85e29964a223af9e867b6f59ecb55ffbf5
erxweo <yizzr81@gmail.com>
2022-06-23 12:02:43 +0400

    Revert "fix roles"
    This reverts commit a8eb5cbc9e20945a77d7890c7331975bf4d9c9d2.

--- app/Models/User.php
+++ app/Models/User.php
@@ -52,4 +52,8 @@ class User extends Authenticatable implements JWTSubject
     {
         return [];
     }
+
+    public function roles() {
+        return $this->belongsToMany(Role::class,'users_roles');
+    }
 }
